ba95a45818ef6e9b6f4c0e09837163b1c77beac2
森林之王 <747273183@qq.com>
2019-10-21 18:03:45 +0800

    2-1 帧动画

--- app/src/main/java/com/example/animationbasic/FrameAnimationActivity.java
+++ app/src/main/java/com/example/animationbasic/FrameAnimationActivity.java
@@ -2,15 +2,35 @@ package com.example.animationbasic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
 
 public class FrameAnimationActivity extends AppCompatActivity {
+
+    private AnimationDrawable animationDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frame_animation);
+
+        View view=findViewById(R.id.view);
+        animationDrawable = (AnimationDrawable) view.getBackground();
+        animationDrawable.setOneShot(true);//动一次
     }
 
 
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.btn_start:
+                animationDrawable.start();
+                break;
+            case R.id.btn_stop:
+                animationDrawable.stop();
+
+                break;
+        }
+    }
 }
